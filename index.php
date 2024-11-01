d318b92c97531e6c0ed3dd2d3c070d36c8cc55c6
soffialorenzo <sofialorenzo1272@gmail.com>
2024-11-01 01:37:06 -0300

    tailwind postcss

--- index.php
+++ index.php
@@ -56,7 +56,6 @@ if(array_key_exists($view,$secciones)){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pristine</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="css/estilos.css">
 </head>
@@ -70,6 +69,5 @@ if(array_key_exists($view,$secciones)){
 </main>
 <?php include_once "includes/footer.php" ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
