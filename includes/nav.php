b35e6a70ed9c86b3172f86c49232644a05d10eb4
sofiialorenzo <sofialorenzo1272@gmail.com>
2024-12-11 11:01:43 -0300

    modal usuario

--- includes/nav.php
+++ includes/nav.php
@@ -58,7 +58,7 @@ $categorias_id = ( new Producto())->categorias_validas();
         <div class="relative bg-white rounded-lg shadow">
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
                 <h3 class="text-xl font-semibold text-gray-900" id="userModalLabel">Bienvenido <?= $_SESSION["login"]['username']; ?>!</h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="userModal">
+                <button type="button" class="text-violet-950 bg-transparent hover:border-2 hover:border-violet-950 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="userModal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
@@ -84,10 +84,10 @@ $categorias_id = ( new Producto())->categorias_validas();
                     <div class="overflow-x-auto">
                         <table class="min-w-full table-auto">
                             <thead>
-                                <tr class="bg-gray-100">
-                                    <th class="px-4 py-2 text-left text-sm font-semibold text-gray-600">Nombre del Producto</th>
-                                    <th class="px-4 py-2 text-left text-sm font-semibold text-gray-600">Cantidad</th>
-                                    <th class="px-4 py-2 text-left text-sm font-semibold text-gray-600">Total</th>
+                                <tr class="bg-violet-100">
+                                    <th class="px-4 py-2 text-left text-sm font-semibold text-violet-800">Nombre del Producto</th>
+                                    <th class="px-4 py-2 text-left text-sm font-semibold text-violet-800">Cantidad</th>
+                                    <th class="px-4 py-2 text-left text-sm font-semibold text-violet-800">Total</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -106,7 +106,7 @@ $categorias_id = ( new Producto())->categorias_validas();
                 <?php endif; ?>
             </div>
             <div class="flex justify-end p-4 border-t">
-                <a href="admin/actions/auth_logout.php" class="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700">Cerrar Sesión</a>
+                <a href="admin/actions/auth_logout.php" class="px-4 py-2 bg-violet-900 hover:bg-transparent hover:border-2 hover:border-violet-950 hover:text-violet-950 text-white font-semibold rounded-lg">Cerrar Sesión</a>
             </div>
         </div>
     </div>
